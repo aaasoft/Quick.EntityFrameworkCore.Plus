8de38fe6289827020874de06eda6a22e0f883cdb
张鹏 <scbeta@qq.com>
2025-01-03 14:58:29 +0800

    Revert "增加一个默认的空的DbContextConfigHandler类的实例。"
    This reverts commit 40246f93585b6400e2e3d0f41f44401d5d3973f5.

--- src/Quick.EntityFrameworkCore.Plus/ConfigDbContext.cs
+++ src/Quick.EntityFrameworkCore.Plus/ConfigDbContext.cs
@@ -17,7 +17,7 @@ namespace Quick.EntityFrameworkCore.Plus
         /// <summary>
         /// 配置处理器
         /// </summary>
-        public static IDbContextConfigHandler ConfigHandler { get; set; } = new EmptyDbContextConfigHandler();
+        public static IDbContextConfigHandler ConfigHandler { get; set; }
 
         public static void Init(IDbContextConfigHandler configHandler, Action<ModelBuilder> modelBuilderHandler)
         {
